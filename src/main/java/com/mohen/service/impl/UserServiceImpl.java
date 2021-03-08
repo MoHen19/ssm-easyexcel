19d2db6bb5261538538cd4077ba05a9ee45f54b8
mohenmh19 <mohenmh19@163.com>
2021-03-08 23:35:22 +0800

    [I] innit project

--- src/main/java/com/mohen/service/impl/UserServiceImpl.java
+++ src/main/java/com/mohen/service/impl/UserServiceImpl.java
@@ -22,4 +22,8 @@ public class UserServiceImpl implements UserService {
         return userMapper.getAll();
     }
 
+    @Override
+    public void saveBatch(List<User> list) {
+        userMapper.saveBatch(list);
+    }
 }
